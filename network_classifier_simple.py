f74a7b040ec8548752d9fc04febf24bec0fd736b
darjwx <darjwx@gmail.com>
2020-12-22 13:25:21 +0100

    network: simple: overhaul statistics and adapt for new label system
    * Per class accuracy and statistics from the metrics function.
    * The network now has two identical outputs. One for each class of 3 labels.

--- network_classifier_simple.py
+++ network_classifier_simple.py
@@ -1,6 +1,7 @@
-# Dataloader
+# Utils and dataloader
 from utils.dataloader_hf import DataLoaderHF
 from utils.transforms import Rescale, ToTensor
+from utils.metrics import get_metrics
 
 # Pytorch
 import torch
@@ -35,7 +36,8 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 53 * 97, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 9)
+        self.fc3 = nn.Linear(84, 3)
+        self.fc4 = nn.Linear(84, 3)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -43,8 +45,9 @@ class Net(nn.Module):
         x = x.view(-1, 16 * 53 * 97)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        out1 = self.fc3(x)
+        out2 = self.fc4(x)
+        return out1, out2
 
 # Detect if we have a GPU available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -55,9 +58,13 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Custom Dataloader for NuScenes
-HOME_ROUTE = ''
+HOME_ROUTE = '/media/darjwx/ssd_data/data/sets/nuscenes/'
 dataset_train = DataLoaderHF(HOME_ROUTE, 'train', 1111, 850, composed)
 dataset_val = DataLoaderHF(HOME_ROUTE, 'val', 1111, 850, composed)
+
+classes_speed = ['stop', 'stoping', 'accel']
+
+classes_steering = ['straight', 'left', 'right']
 
 trainloader = DataLoader(dataset_train, batch_size, shuffle=False, num_workers=4)
 valloader = DataLoader(dataset_val, batch_size, shuffle=False, num_workers=4)
@@ -72,8 +79,11 @@ for epoch in range(num_epochs):
         images = images.to(device)
         labels = labels.to(device)
 
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+        out1, out2 = model(images)
+
+        loss1 = criterion(out1, labels[:, 0])
+        loss2 = criterion(out2, labels[:, 1])
+        loss = loss1 + loss2
 
         optimizer.zero_grad()
         loss.backward()
@@ -81,8 +91,8 @@ for epoch in range(num_epochs):
 
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+            print('[%d, %5d] loss: %.3f'
+                 % (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
 
 print('Finished training')
@@ -92,8 +102,21 @@ print('Finished training')
 # torch.save(model.state_dict(), PATH)
 
 print('Validating with %d' % (len(dataset_val)))
-correct = 0
-total = 0
+
+class_correct_1 = list(0. for i in range(3))
+class_total_1 = list(0. for i in range(3))
+class_correct_2 = list(0. for i in range(3))
+class_total_2 = list(0. for i in range(3))
+
+all_preds_1 = torch.tensor([])
+all_preds_2 = torch.tensor([])
+all_labels_1 = torch.tensor([])
+all_labels_2 = torch.tensor([])
+all_preds_1 = all_preds_1.to(device)
+all_preds_2 = all_preds_2.to(device)
+all_labels_1 = all_labels_1.to(device)
+all_labels_2 = all_labels_2.to(device)
+
 with torch.no_grad():
     for data in valloader:
         images = data['image']
@@ -102,10 +125,38 @@ with torch.no_grad():
         images = images.to(device)
         labels = labels.to(device)
 
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        out1, out2 = model(images)
+        _, predicted_1 = torch.max(out1.data, 1)
+        correct_1 = (predicted_1 == labels[:, 0]).squeeze()
+        _, predicted_2 = torch.max(out2.data, 1)
+        correct_2 = (predicted_2 == labels[:, 1]).squeeze()
 
-print('Accuracy of the network: %d %%'
-       % (100 * correct / total))
+        all_preds_1 = torch.cat((all_preds_1, predicted_1), dim=0)
+        all_preds_2 = torch.cat((all_preds_2, predicted_2), dim=0)
+
+        all_labels_1 = torch.cat((all_labels_1, labels[:, 0]), dim=0)
+        all_labels_2 = torch.cat((all_labels_2, labels[:, 1]), dim=0)
+
+        for i in range(batch_size):
+            label = labels[i,0]
+            class_correct_1[label] += correct_1[i].item()
+            class_total_1[label] +=1
+
+            label = labels[i,1]
+            class_correct_2[label] += correct_2[i].item()
+            class_total_2[label] += 1
+
+
+#Per class statistics
+#Accuracy
+for i in range(3):
+    accuracy_speed = class_correct_1[i] / class_total_1[i]
+    accuracy_steering = class_correct_2[i] / class_total_2[i]
+    print('Accuracy of %5s: %1.3f'
+          % (classes_speed[i], accuracy_speed))
+    print('Accuracy of %5s: %1.3f'
+          % (classes_steering[i], accuracy_steering))
+
+#Recall, precision, f1_score and confusion_matrix
+get_metrics(all_labels_1.cpu(), all_preds_1.cpu(), 3, classes_speed)
+get_metrics(all_labels_2.cpu(), all_preds_2.cpu(), 3, classes_steering)
