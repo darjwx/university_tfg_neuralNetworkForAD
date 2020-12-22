df12ff0c07ed0998faf64ec881ae9f47fd60e27e
darjwx <darjwx@gmail.com>
2020-12-22 12:30:57 +0100

    utils: add a metrics function
    * It uses sklearn.metrics
    * As of now supports the following metrics:
    	recall, precision, f1 score and confusion matrix.
    * Work in progress.

---
+++ utils/metrics.py
@@ -0,0 +1,17 @@
+#sklearn
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
+
+
+def get_metrics(labels_true, labels_pred, num_classes, classes):
+
+    recall = recall_score(labels_true, labels_pred, average=None)
+    precision = precision_score(labels_true, labels_pred, average=None)
+    f1 = f1_score(labels_true, labels_pred, average=None)
+    conf_matrix = confusion_matrix(labels_true, labels_pred)
+
+    for i in range(num_classes):
+        print('Recall, precision and F1-score of %5s: %1.3f %1.3f %1.3f'
+             % (classes[i], recall[i], precision[i], f1[i]))
+
+    print('--Confusion matrix--')
+    print(conf_matrix)
