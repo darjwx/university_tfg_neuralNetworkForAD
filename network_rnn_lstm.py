6820ef871b77cf458a87823c51a03120f68b7bfd
darjwx <darjwx@gmail.com>
2021-02-13 13:11:10 +0100

    network: lstm: update the model
    * Remove the visualize_model function: OOM error caused
      by the model not being deleted.
    * Use only 1 layer for now. 2 needs testing.
    * Use weights in criterion since we are working with and imbalanced dataset.
    * Use view instead of reshape.
    * Increase the Conv parameters.
    * Use SGD with momentum with the option to use Adam.
    * Disable the lr schedulerfor now.
    * Zero the whole model instead of just the optimizer.
    
    TODO: improve val loss and test different optimizers for each output.

--- network_rnn_lstm.py
+++ network_rnn_lstm.py
@@ -3,7 +3,7 @@ from utils.dataloader_seq import DataLoaderSeq
 from utils.transforms import Rescale, ToTensor, Normalize
 
 # Metrics
-from utils.metrics import get_metrics, show_predicted_data, visualize_model, update_scalar_tb, pr_curve_tb, dummy_classifier
+from utils.metrics import get_metrics, show_predicted_data, update_scalar_tb, pr_curve_tb, dummy_classifier
 
 # Pytorch
 import torch
@@ -12,6 +12,7 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
@@ -26,7 +27,7 @@ torch.manual_seed(1)
 
 # Parameters
 input_size = 84
-num_layers = 2
+num_layers = 1
 hidden_size = 128
 num_epochs = 15
 batch_size = 1
@@ -46,32 +47,34 @@ class CNNtoLSTM(nn.Module):
         super(CNNtoLSTM, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.conv1 = nn.Conv2d(3, 6, 5)
+
+        # Conv layers
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.conv2 = nn.Conv2d(16, 32, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 53 * 97, 120)
+        self.fc1 = nn.Linear(32 * 53 * 97, 120)
         self.fc2 = nn.Linear(120, 84)
+
+        # LSTM and output linear layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc3 = nn.Linear(hidden_size, num_classes)
         self.fc4 = nn.Linear(hidden_size, num_classes)
 
-    def forward(self, x):
-        hidden = (torch.zeros(self.num_layers, x.size(0), self.hidden_size, device=device, requires_grad=True),
-                  torch.zeros(self.num_layers, x.size(0), self.hidden_size, device=device, requires_grad=True))
 
-        x = x.reshape(-1, 3, 225, 400)
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+        x = x.view(-1, 3, 225, 400)
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 53 * 97)
+        x = x.view(-1, 32 * 53 * 97)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = x.reshape(batch_size, sequence_length, -1)
-        out1, _ = self.lstm(x, hidden)
-        out1 = out1.reshape(-1, hidden_size)
-        out1 = self.fc3(out1)
-        out2, _ = self.lstm(x, hidden)
-        out2 = out2.reshape(-1, hidden_size)
-        out2 = self.fc4(out2)
+        x = x.view(batch_size, x.size(0), -1)
+        x, _ = self.lstm(x, (h0, c0))
+        out1 = self.fc3(x.view(-1, hidden_size))
+        out2 = self.fc4(x.view(-1, hidden_size))
 
         return out1, out2
 
@@ -81,10 +84,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = CNNtoLSTM(input_size, hidden_size, num_layers, num_classes)
 model = model.to(device)
 
-criterion = nn.CrossEntropyLoss()
-#optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-scheduler = ReduceLROnPlateau(optimizer, 'max', 0.1, 2, verbose = True)
+weights = torch.tensor([1., 6.67, 6.29], device=device)
+criterion = nn.CrossEntropyLoss(weight=weights)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+# scheduler = ReduceLROnPlateau(optimizer, 'min', 0.1, 2, verbose = True)
 
 # Custom Dataloader for NuScenes
 HOME_ROUTE = '/media/darjwx/ssd_data/data/sets/nuscenes/'
@@ -103,21 +107,19 @@ print('Training with %d groups of connected images' % (len(dataset_train)))
 for epoch in range(num_epochs):
     rloss1 = 0.0
     rloss2 = 0.0
+
     for i, data in enumerate(trainloader):
 
-        optimizer.zero_grad()
+        model.zero_grad()
         images = data['image']
         labels = data['label']
 
         images = images.to(device)
         labels = labels.to(device)
 
-        sequence_length = images.size(1)
         out1, out2 = model(images)
 
-        visualize_model(model, images)
-
-        labels = labels.reshape(-1, 2)
+        labels = labels.view(-1, 2)
         loss1 = criterion(out1, labels[:, 0])
         loss2 = criterion(out2, labels[:, 1])
         loss = loss1 + loss2
@@ -139,32 +141,23 @@ for epoch in range(num_epochs):
             rloss1 = 0.0
             rloss2 = 0.0
 
+    # Validation loss
+    with torch.no_grad():
+        for i, data in enumerate(valloader):
+            images = data['image']
+            labels = data['label']
 
-    # Val acc for the scheduler step
-    correct1 = 0
-    correct2 = 0
-    for data in valloader:
-        images = data['image']
-        labels = data['label']
+            images = images.to(device)
+            labels = labels.to(device)
 
-        images = images.to(device)
-        labels = labels.to(device)
+            labels = labels.view(-1, 2)
 
-        sequence_length = images.size(1)
-        labels = labels.reshape(-1, 2)
+            out1, out2 = model(images)
+            loss1_val = criterion(out1, labels[:, 0])
+            loss2_val = criterion(out2, labels[:, 1])
 
-        out1, out2 = model(images)
-
-        _, predicted_1 = torch.max(out1.data, 1)
-        _, predicted_2 = torch.max(out2.data, 1)
-        correct1 += (predicted_1 == labels[:, 0]).sum().item()
-        correct2 += (predicted_2 == labels[:, 1]).sum().item()
-
-    acc1 = correct1 / len(valloader)
-    acc2 = correct2 / len(valloader)
-    acc = (acc1 + acc2) / 2
-
-    scheduler.step(acc)
+            update_scalar_tb('validation loss speed', loss1_val, epoch * len(valloader) + i)
+            update_scalar_tb('validation loss direction', loss2_val, epoch * len(valloader) + i)
 
 
 print('Finished training')
@@ -200,8 +193,7 @@ with torch.no_grad():
         images = images.to(device)
         labels = labels.to(device)
 
-        sequence_length = images.size(1)
-        labels = labels.reshape(-1, 2)
+        labels = labels.view(-1, 2)
 
         out1, out2 = model(images)
         _, predicted_1 = torch.max(out1.data, 1)
@@ -237,14 +229,14 @@ accuracy_t_2 = correct_total_2 / len(valloader.dataset)
 print('Accuracy 1: %1.3f  Accuracy 2: %1.3f'
       % (accuracy_t_1, accuracy_t_2))
 
-#Per class statistics
+# Per class statistics
 # Dummy classifier: most_frequent and constant
 print('Dummy classifier 1')
 dummy_classifier(all_labels_1.cpu(), 1)
 print('Dummy classifier 2')
 dummy_classifier(all_labels_2.cpu(), 1)
 
-#Accuracy
+# Accuracy
 for i in range(3):
     accuracy_speed = class_correct_1[i] / class_total_1[i]
     accuracy_steering = class_correct_2[i] / class_total_2[i]
@@ -259,6 +251,6 @@ preds_2 = torch.cat([torch.stack(batch) for batch in preds_2])
 
 pr_curve_tb(3, all_labels_1, all_labels_2, preds_1, preds_2)
 
-#Recall, precision, f1_score and confusion_matrix
+# Recall, precision, f1_score and confusion_matrix
 get_metrics(all_labels_1.cpu(), all_preds_1.cpu(), 3, classes_speed)
 get_metrics(all_labels_2.cpu(), all_preds_2.cpu(), 3, classes_steering)
