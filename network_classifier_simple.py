1a13ddabb324df9493f451ccd9bcfbed2539f4c5
darjwx <darjwx@gmail.com>
2021-06-24 23:38:40 +0200

    project: add a license
    * GPL v2

--- network_classifier_simple.py
+++ network_classifier_simple.py
@@ -1,3 +1,21 @@
+"""
+    Simple classifier - Classifier designed to be light.
+    Copyright (C) 2020-2021  Darío Jiménez
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, see <http://www.gnu.org/licenses/>.
+"""
+
 # Utils and dataloader
 from utils.dataloader_hf import DataLoaderHF
 from utils.transforms import Rescale, ToTensor, Normalize
